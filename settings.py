e572527de063598a85fb1d253dcad87b9cc89ca6
mxm <2243014395@qq.com>
2026-03-23 14:50:12 +0800

    auto-commit

--- settings.py
+++ settings.py
@@ -18,10 +18,20 @@ INSTALLED_APPS = [
     'restaurant',
 ]
 
+
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'smart_restaurant',      
+        'USER': 'root',                 
+        'PASSWORD': '123456',       
+        'HOST': '127.0.0.1',            
+        'PORT': '3306',               
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        }
     }
 }
 
